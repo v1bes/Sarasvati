35bd4529f8911963a46c3578c3e6d6b75da40d9d
Henry Schober <github@henry-schober.com>
2017-05-06 15:55:45 +0200

    Photoswipe gallery finished, testing with button.
    Image slide to be implemented

--- js/photoswipe/init.js
+++ js/photoswipe/init.js
@@ -1,26 +1,54 @@
-var pswpElement = document.querySelectorAll('.pswp')[0];
+var openPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
 
-// build items array
-var items = [
-    {
-        src: 'https://placekitten.com/600/400',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'https://placekitten.com/1200/900',
-        w: 1200,
-        h: 900
-    }
-];
+    // build items array
+    var items = [
+        {
+            src: 'img/gallery/watt1.jpg',
+            w: 525,
+            h: 366
+        },
+        {
+            src: 'img/gallery/watt2.jpg',
+            w: 1252,
+            h: 1252
+        },
+        {
+            src: 'img/gallery/watt3.jpg',
+            w: 703,
+            h: 466
+        },
+        {
+            src: 'img/gallery/watt4.jpg',
+            w: 800,
+            h: 800
+        },
+        {
+            src: 'img/gallery/watt5.jpg',
+            w: 524,
+            h: 366
+        },
+        {
+            src: 'img/gallery/watt6.jpg',
+            w: 480,
+            h: 360
+        }
+    ];
 
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
+    // define options (if needed)
+    var options = {
+             // history & focus options are disabled on CodePen
+        history: false,
+        focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
 };
 
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
+// openPhotoSwipe();
+document.getElementById('psbtn').onclick = openPhotoSwipe;
