9a1c222755f1490b48305a29bc6faa9a2f237fe2
Henrosaurus <git@henry-schober.com>
2017-05-13 17:37:38 +0200

    processwire gallery integration

--- site/templates/home.php
+++ site/templates/home.php
@@ -79,132 +79,7 @@ include("./basic-page.php");?>
       </div>
     </section>
 
-<!-- Gallerie -->
-<!-- Photoswipe -->
-    <!-- Core JS file -->
-        <script src="<?php echo $config->urls->templates?>js/photoswipe/photoswipe.min.js"></script>
-    <!-- UI JS file -->
-        <script src="<?php echo $config->urls->templates?>js/photoswipe/ui/photoswipe-ui-default.js"></script>
-  <!-- photoswipe dom -->
-      <!-- Root element of PhotoSwipe. Must have class pswp. -->
-    <button id="psbtn" class="center">TEST GALLERY</button>
-    <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-      <!-- Background of PhotoSwipe.
-             It's a separate element as animating opacity is faster than rgba(). -->
-
-        <div class="pswp__bg"></div>
-      <!-- Slides wrapper with overflow:hidden. -->
-        <div class="pswp__scroll-wrap">
-          <!-- Container that holds slides.
-                PhotoSwipe keeps only 3 of them in the DOM to save memory.
-                Don't modify these 3 pswp__item elements, data is added later on. -->
-
-            <div class="pswp__container">
-                <div class="pswp__item"></div>
-                <div class="pswp__item"></div>
-                <div class="pswp__item"></div>
-            </div>
-          <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
-            <div class="pswp__ui pswp__ui--hidden">
-                <div class="pswp__top-bar">
-                  <!--  Controls are self-explanatory. Order can be changed. -->
-                    <div class="pswp__counter"></div>
-                    <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-                    <button class="pswp__button pswp__button--share" title="Share"></button>
-                    <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-                    <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-                    <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
-                  <!-- element will get class pswp__preloader--active when preloader is running -->
-                    <div class="pswp__preloader">
-                        <div class="pswp__preloader__icn">
-                          <div class="pswp__preloader__cut">
-                            <div class="pswp__preloader__donut"></div>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                    <div class="pswp__share-tooltip"></div>
-                </div>
-                <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-                </button>
-                <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-                </button>
-                <div class="pswp__caption">
-                    <div class="pswp__caption__center"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-  <!--End of photoswipe dom -->
-<script>
-    var openPhotoSwipe = function() {
-        var pswpElement = document.querySelectorAll('.pswp')[0];
-
-        // build items array
-        var items = [
-            {
-                src: '<?php echo $config->urls->templates?>img/gallery/watt1.jpg',
-                w: 525,
-                h: 366
-            },
-            {
-                src: '<?php echo $config->urls->templates?>img/gallery/watt2.jpg',
-                w: 1252,
-                h: 1252
-            },
-            {
-                src: '<?php echo $config->urls->templates?>img/gallery/watt3.jpg',
-                w: 703,
-                h: 466
-            },
-            {
-                src: '<?php echo $config->urls->templates?>img/gallery/watt4.jpg',
-                w: 800,
-                h: 800
-            },
-            {
-                src: '<?php echo $config->urls->templates?>img/gallery/watt5.jpg',
-                w: 524,
-                h: 366
-            },
-            {
-                src: '<?php echo $config->urls->templates?>img/gallery/watt6.jpg',
-                w: 480,
-                h: 360
-            }
-        ];
-
-        // define options (if needed)
-        var options = {
-                 // history & focus options are disabled on CodePen
-            history: false,
-            focus: false,
-
-            showAnimationDuration: 0,
-            hideAnimationDuration: 0
-
-        };
-
-        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-        gallery.init();
-    };
-
-    // openPhotoSwipe();
-    document.getElementById('psbtn').onclick = openPhotoSwipe;
-</script>
-    <section class="gallery">
-      <div>
-        <!-- TODO: PHP Loop für images aus processwire field -->
-        <img src="<?php echo $config->urls->templates?>img/gallery/watt1.jpg">
-        <img src="<?php echo $config->urls->templates?>img/gallery/watt2.jpg">
-        <img src="<?php echo $config->urls->templates?>img/gallery/watt3.jpg">
-        <img src="<?php echo $config->urls->templates?>img/gallery/watt4.jpg">
-        <img src="<?php echo $config->urls->templates?>img/gallery/watt5.jpg">
-        <img src="<?php echo $config->urls->templates?>img/gallery/watt6.jpg">
-      </div>
-    </section>
-  <!-- End of gallery-->
+<?php include("./php/gallery.php");?>
 
     <!-- Newsletter -->
     <section class="newsletter">
@@ -225,7 +100,7 @@ include("./basic-page.php");?>
       <i class="fa fa-instagram" aria-hidden="true"></i>
     </div>
   </footer>
-
+<!--
   <script src="<?php echo $config->urls->templates?>js/regl.min.js"></script>
   <script src="<?php echo $config->urls->templates?>js/min/function-min.js"></script>
   <script id="fragmentShader" type="x-shader/x-fragment">
@@ -301,6 +176,6 @@ include("./basic-page.php");?>
     }
   </script>
   <script src="<?php echo $config->urls->templates?>js/demo1.js"></script>
-
+-->
 </body>
 </html>
